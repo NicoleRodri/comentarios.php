bd35db4addd2f992fdd040434ea9ea710d8690e1
NicoleRodri <107197293+NicoleRodri@users.noreply.github.com>
2022-06-30 21:54:36 -0500

    Create comentarios.php

---
+++ comentarios.php
@@ -0,0 +1,21 @@
+<?php 
+/**
+*
+*/
+clase  Comentarios 
+require_once (" Conexion.php ");
+clase  Comentarios   extiende  Conexión
+{
+
+	función  __construir ( argumento )
+	{
+		// código...
+	}
+
+	public  function  listar_comentarios (){
+
+	}
+
+}
+
+?> 
